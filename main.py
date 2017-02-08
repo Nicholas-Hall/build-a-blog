3080db7def73029828b07ecc8350a0dea0cb2909
Nicholas Hall <NickJohnHall@gmail.com>
2017-02-07 19:45:51 -0600

    finished rough code with jesse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#
 # Copyright 2007 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +16,7 @@
 import os
 import webapp2
 import jinja2
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
@@ -32,9 +32,10 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainHandler(Handler):
+class FrontPageHandler(Handler):
     def get(self):
-        self.render("new_post_views.html")
+        content = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        self.render("frontpage.html", content = content)
 
 class NewPost(Handler):
     def get(self):
@@ -46,9 +47,11 @@ class NewPost(Handler):
         error_title = ""
         error_content = ""
 
-
         if title and blogcontent:
-            self.write("hey")
+            blog = Post(title = title, blogcontent = blogcontent)
+            blog.put()
+            
+            self.redirect("/blog/" + str(blog.key().id()))
         else:
             if title:
                 error_content = "We need Content"
@@ -56,8 +59,24 @@ class NewPost(Handler):
                 error_title = "We need title"
             self.render("new_post.html",title = title, blogcontent = blogcontent, error_content = error_content, error_title = error_title)
 
+class Post(db.Model):
+    title = db.StringProperty(required =True)
+    blogcontent = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        content = Post.get_by_id(int(id))
+
+        self.render("permalink.html",content = content)
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/blog', MainHandler),
+    ('/', FrontPageHandler),
+    ('/blog', FrontPageHandler),
     ('/newpost', NewPost),
+    (webapp2.Route('/blog/<id:\d+>', ViewPostHandler)),
 ], debug=True)
+
+# Still need to add a dynamic web page with 5 newest posted
+#each post
